97759bd4ce4445ad073b1e6bee32f5c832fb9932
Leila Kasmi <leilakasmi150@gmail.com>
2024-05-05 12:41:04 +0100

    Data Mining

--- CAH/CAHProject.java
+++ CAH/CAHProject.java
@@ -67,9 +67,64 @@ public class CAHProject extends JFrame {
             htmlContent.append("</tr>");
         }
 
-        htmlContent.append("</table><h1>Intra Class</h1></body></html>");
+        htmlContent.append("</table>");
 
+        
+        // ==================== Intra Class ===========================
+        classDataList = new ArrayList<>();
+         for (double value : classValues) {
+            String s = String.valueOf(value);
+            classDataList.add(new ClassData(1, value, value, s));
+        }
+        htmlContent.append("<h1>Intra Class</h1><table border='1' width='100%'>");
+        htmlContent.append("<tr><th colspan='" + nbrClass + "'>Classes</th></tr><tr>");
+         
+         for (ClassData classData : classDataList) {
+            htmlContent.append("<td>").append(classData.getVal()).append("</td>");
+        }
+         htmlContent.append("</tr>");
+        
+         
+       numClass = classDataList.size();
+       while (numClass > 1) {
+           double minDistance = Double.MAX_VALUE;
+           int minIndex1 = -1;
+           int minIndex2 = -1;
 
+            for (int l = 0; l < classDataList.size(); l++) {
+                for (int k = l + 1; k < classDataList.size(); k++) { 
+                    ClassData class1 = classDataList.get(l);
+                    ClassData class2 = classDataList.get(k);
+                    double distance = Math.abs(class1.fv - class2.lv);
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        minIndex1 = l;
+                        minIndex2 = k;
+                    }
+                }
+            }
+    
+            ClassData class1 = classDataList.get(minIndex1);
+            ClassData class2 = classDataList.get(minIndex2);
+    
+            class1.nc += class2.nc;
+            class1.lv = class2.lv;
+            class1.val += " | " + class2.val;
+            classDataList.remove(minIndex2);
+            numClass--;
+
+            htmlContent.append("<tr>");
+            for (ClassData cd : classDataList) {
+                htmlContent.append("<td colspan='").append(cd.getNc()).append("'>").append(cd.getVal()).append("</td>");
+            }
+            htmlContent.append("</tr>");
+        }
+
+         
+         
+         htmlContent.append("</table>");
+        
+        htmlContent.append("</body></html>");
         JEditorPane editorPane = new JEditorPane("text/html", htmlContent.toString());
         editorPane.setEditable(false);
 
